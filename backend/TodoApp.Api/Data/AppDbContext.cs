dc13c9e1f2593010b97ad0579d19271b5b13988b
Durid Ahmad <durid.ahmad.dev@gmail.com>
2026-06-23 20:57:51 -0400

    update database with Todo entity

--- backend/TodoApp.Api/Data/AppDbContext.cs
+++ backend/TodoApp.Api/Data/AppDbContext.cs
@@ -1,8 +1,24 @@
 using Microsoft.EntityFrameworkCore;
+using TodoApp.Api.Models;
 
 namespace TodoApp.Api.Data;
 
 public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Todo> Todos => Set<Todo>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Todo>(entity =>
+        {
+            entity.Property(t => t.Title)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            entity.Property(t => t.CreatedAt)
+                .HasDefaultValueSql("CURRENT_TIMESTAMP");
+        });
+    }
 }
--- backend/TodoApp.Api/Program.cs
+++ backend/TodoApp.Api/Program.cs
@@ -20,6 +20,11 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    scope.ServiceProvider.GetRequiredService<AppDbContext>().Database.Migrate();
+}
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
